1a249567146208ef1064d1728ccc09fa30efcb5d
Arthur Gunn <arthur@gunn.co.nz>
2011-05-27 19:49:20 +1200

    Table name: reader_group_payment is actually reader_group_payments.

--- vendor/extensions/nzffa_site/db/migrate/20110527084328_add_year_for_to_reader_group_payments.rb
+++ vendor/extensions/nzffa_site/db/migrate/20110527084328_add_year_for_to_reader_group_payments.rb
@@ -1,9 +1,9 @@
 class AddYearForToReaderGroupPayments < ActiveRecord::Migration
   def self.up
-    add_column :reader_group_payment, :year_for, :integer
+    add_column :reader_group_payments, :year_for, :integer
   end
 
   def self.down
-    remove_column :reader_group_payment, :year_for
+    remove_column :reader_group_payments, :year_for
   end
 end
