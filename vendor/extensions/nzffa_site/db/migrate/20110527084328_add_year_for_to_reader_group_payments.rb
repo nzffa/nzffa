96205159d116cd0398e6f382e70003cc7c76498a
Arthur Gunn <arthur@gunn.co.nz>
2011-05-27 19:39:49 +1200

    Added migration.

---
+++ vendor/extensions/nzffa_site/db/migrate/20110527084328_add_year_for_to_reader_group_payments.rb
@@ -0,0 +1,9 @@
+class AddYearForToReaderGroupPayment < ActiveRecord::Migration
+  def self.up
+    add_column :reader_group_payment, :year_for, :integer
+  end
+
+  def self.down
+    remove_column :reader_group_payment, :year_for
+  end
+end
