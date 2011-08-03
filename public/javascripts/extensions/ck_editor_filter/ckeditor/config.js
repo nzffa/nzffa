9477dd195e48d6fbafbe782414e7c9c8986c44b3
Arthur Gunn <arthur@gunn.co.nz>
2011-08-03 15:18:33 +1200

    Added custom config for ckeditor to customise toolbar.

---
+++ public/javascripts/extensions/ck_editor_filter/ckeditor/config.js
@@ -0,0 +1,26 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+  config.toolbar = 'CustomToolbar';
+
+  config.toolbar_CustomToolbar =
+  [
+  	{ name: 'styles', items : [ 'Format' ] },
+  	{ name: 'basicstyles', items : [ 'Bold','Italic','Subscript','Superscript' ] },
+  	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+  	'/',
+  	{ name: 'tools', items : [ 'Maximize', 'ShowBlocks'] },
+  	{ name: 'clipboard', items : [ 'PasteText','PasteFromWord'] },
+  	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  	{ name: 'insert', items : [ 'Image','Table','HorizontalRule','SpecialChar','PageBreak' ] },
+  	{ name: 'colors', items : [ 'TextColor','BGColor' ] }
+  ];
+  
+};
