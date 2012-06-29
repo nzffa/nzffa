92b46f4a1595c20e69ae20eca8d7ee93752c0a8d
Robert Guthrie <rguthrie@gmail.com>
2012-06-29 15:10:54 +1200

    small updates.. radius tags

--- vendor/extensions/marketplace/lib/marketplace.rb
+++ vendor/extensions/marketplace/lib/marketplace.rb
@@ -7,4 +7,23 @@ module Marketplace
       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
       <script type="text/javascript">$('#adverts').load('#{adverts_path}')</script>}
   end
+
+  tag 'my_adverts' do |tag|
+    %{<div id="adverts"></div>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+      <script type="text/javascript">$('#adverts').load('#{my_adverts_adverts_path}')</script>}
+  end
+
+  tag 'edit_company_listing' do |tag|
+    %{<div id="adverts"></div>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+      <script type="text/javascript">$('#adverts').load('#{edit_company_listing_adverts_path}')</script>}
+  end
+
+  tag 'new_advert' do |tag|
+    %{<div id="adverts"></div>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+      <script type="text/javascript">$('#adverts').load('#{new_advert_path}')</script>}
+  end
+
 end
