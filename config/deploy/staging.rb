6f956808264078d91ac52956b2dd32574c94e480
Robert Guthrie <rguthrie@gmail.com>
2012-08-20 16:47:54 +1200

    unproved deployment fixes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,16 +7,3 @@ role :web, solo_host
 role :app, solo_host
 role :db,  solo_host, :primary => true
 
-set :use_sudo, false
-
-set :ssh_options, {:forward_agent => true}
-
-task :setup_env do
-  run "RACK_ENV=#{rails_env}"
-  run "RAILS_ENV=#{rails_env}"
-
-  run "echo 'RackEnv #{rails_env}' >> #{File.join(current_path, '.htaccess')}"
-  run "echo 'RailsEnv #{rails_env}' >> #{File.join(current_path, '.htaccess')}"
-end
-
-before "restart", "setup_env"
