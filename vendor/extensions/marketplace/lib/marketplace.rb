dad1c06b4c5a3bbd98e38d4bd4ff5c7c7ddddb79
Robert Guthrie <rguthrie@gmail.com>
2012-09-17 19:41:44 +1200

    text changes and add hello_or_login

--- vendor/extensions/marketplace/lib/marketplace.rb
+++ vendor/extensions/marketplace/lib/marketplace.rb
@@ -2,14 +2,23 @@ module Marketplace
   include Radiant::Taggable
   desc 'Tags to load marketplace into the current page'
 
+  tag 'hello_or_login' do |tag|
+    current_reader = Reader.current
+    if current_reader
+      "Hello #{current_reader.forename}. <a href='/account/'>Your Account</a>. <a href='/subscriptions/'>Your Subscription</a>. <a href='/account/logout'>Logout</a>"
+    else
+      "<a href='/account/login'>Login</a> or <a href='/directory/readers/new'>Signup</a>"
+    end
+  end
+
   tag 'adverts' do |tag|
     %{<div id="adverts"></div>
       <script type="text/javascript">$('#adverts').load('#{adverts_path}')</script>}
   end
 
   tag 'my_adverts' do |tag|
-    %{<div id="adverts"></div>
-      <script type="text/javascript">$('#adverts').load('#{my_adverts_adverts_path}')</script>}
+        %{<div id="adverts"></div>
+          <script type="text/javascript">$('#adverts').load('#{my_adverts_adverts_path}')</script>}
   end
 
   tag 'edit_company_listing' do |tag|
@@ -18,13 +27,13 @@ module Marketplace
   end
 
   tag 'new_advert' do |tag|
-    %{<div id="adverts"></div>
-      <script type="text/javascript">$('#adverts').load('#{new_advert_path}')</script>}
+  %{<div id="adverts"></div>
+    <script type="text/javascript">$('#adverts').load('#{new_advert_path}')</script>}
   end
 
   tag 'register' do |tag|
-    %{<div id="adverts"></div>
-      <script type="text/javascript">$('#adverts').load('/membership/register')</script>}
+        %{<div id="adverts"></div>
+          <script type="text/javascript">$('#adverts').load('/membership/register')</script>}
   end
 
   tag 'join-fft-button' do |tag|
