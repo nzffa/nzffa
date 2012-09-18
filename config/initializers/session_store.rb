ace209ed55c5145075b5206cc0ae08973c86de53
Robert Guthrie <rguthrie@gmail.com>
2012-09-18 15:11:15 +1200

    change session store secret

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_nzffa_session',
+  :secret      => 'blablabla098765456789'
+}
