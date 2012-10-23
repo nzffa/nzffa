d2cb9c9d546097c830c78ba7d8fa9196f3966675
Robert Guthrie <rguthrie@gmail.com>
2012-10-23 20:56:21 +1300

    remove reader dashboard override

--- vendor/extensions/marketplace/config/routes.rb
+++ vendor/extensions/marketplace/config/routes.rb
@@ -12,8 +12,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # this breaks 
   # map.reader_dashboard '/membership/dashboard', :controller => :membership, :action => :dashboard
+  # map.register '/membership/register', :controller => :membership, :action => :register
 
-  map.register '/membership/register', :controller => :membership, :action => :register
   map.join_fft_button '/membership/join-fft-button', :controller => :membership, :action => :join_fft_button
   map.join_fft '/membership/join-fft', :controller => :membership, :action => :join_fft
 
