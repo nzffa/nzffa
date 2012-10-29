81f02e39abee99176407a59d5c5b16bac0534000
Robert Guthrie <rguthrie@gmail.com>
2012-10-29 23:19:30 +1300

    forget updating counter cache.. too much hassle

--- db/migrate/20121029093904_fix_counter_cache_column.rb
+++ db/migrate/20121029093904_fix_counter_cache_column.rb
@@ -1,9 +1,6 @@
 class FixCounterCacheColumn < ActiveRecord::Migration
   def self.up
     change_column :forums, :topics_count, :integer, :null => false, :default => 0
-    Forum.each do |f|
-      Forum.reset_counters(f.id, :topics)
-    end
   end
 
   def self.down
