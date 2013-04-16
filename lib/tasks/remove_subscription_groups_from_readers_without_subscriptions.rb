f0b1d2b7949b17ee829fc6d4242e4f2c3bea0475
Robert Guthrie <rguthrie@gmail.com>
2013-04-16 17:44:22 +1200

    add more group_ids

--- lib/tasks/remove_subscription_groups_from_readers_without_subscriptions.rb
+++ lib/tasks/remove_subscription_groups_from_readers_without_subscriptions.rb
@@ -1,4 +1,5 @@
-subscription_group_ids = [229, 80, 232, 230, 211]
+subscription_group_ids = [11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 80, 201, 209, 211, 217, 221, 225, 229, 230, 232]
+
 @readers = Reader.all.select do |r| 
   if !r.active_subscription and r.group_ids.any?{|id| subscription_group_ids.include?(id) }
     r.groups -= subscription_group_ids
--- vendor/extensions/marketplace/app/controllers/admin/reports_controller.rb
+++ vendor/extensions/marketplace/app/controllers/admin/reports_controller.rb
@@ -13,7 +13,8 @@ class Admin::ReportsController < AdminController
     #nzffa_members_newsletter_group_id = 211
     #Branch.all.map(&:group_id)
     #ActionGroup.all.map(&:group_id)
-    subscription_group_ids = [229, 80, 232, 230, 211]
+    #subscription_group_ids = [229, 80, 232, 230, 211]
+    subscription_group_ids = [11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 80, 201, 209, 211, 217, 221, 225, 229, 230, 232]
     csv_string = FasterCSV.generate do |csv|
       csv << %w[id nzffa_membership_id name email expired_on group_ids]
       @readers = Reader.all.select do |r| 
