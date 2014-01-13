0d88e1c44ceca537fbd65da5467ff50a57209ec0
Robert Guthrie <rob@guthr.ie>
2014-01-13 18:09:29 +1300

    add last year members csv

--- vendor/extensions/marketplace/app/controllers/branch_admin_controller.rb
+++ vendor/extensions/marketplace/app/controllers/branch_admin_controller.rb
@@ -30,6 +30,24 @@ class BranchAdminController < MarketplaceController
     render_csv_of_readers
   end
 
+  def last_year_members_csv
+    @group = Group.find(params[:group_id])
+    @branch = Branch.find(:first, :conditions => {:group_id => @group.id})
+
+    start_of_last_year = 1.year.ago.at_beginning_of_year
+    end_of_last_year= 1.year.ago.at_end_of_year
+    subscription_branches = SubscriptionsBranch.find(:all, :joins => :subscription, :conditions => ['branch_id = ? and (subscriptions.begins_on > ? and subscriptions.expires_on < ?)', @branch.id, start_of_last_year, end_of_last_year])
+    subscription_ids = subscription_branches.map(&:subscription_id)
+    subscriptions = Subscription.find(:all, :conditions => {:id => subscription_ids})
+
+    all_reader_ids = subscriptions.map(&:reader_id)
+    current_reader_ids = @group.reader_ids
+
+    @readers = Reader.find(all_reader_ids - current_reader_ids)
+
+    render_csv_of_readers
+  end
+
   def email
     @group = Group.find(params[:group_id])
 
