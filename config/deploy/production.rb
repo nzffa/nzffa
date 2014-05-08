8d5e97b66ce4fb93ab821d7c7aef76fbeafc33ad
Craig Ambrose <craig@craigambrose.com>
2014-05-09 11:53:44 +1200

    update deploy script for new server location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :rack_env, rails_env
 set :branch, 'production'
 set :deploy_to, "/home/#{user}/#{rails_env}"
 
-set :solo_host, '119.47.122.22'
+set :solo_host, 'nzffa.org.nz'
 role :web, solo_host
 role :app, solo_host
 role :db,  solo_host, :primary => true
