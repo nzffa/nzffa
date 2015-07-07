26edb23c0ef206c9082c90fa1ebc99b61112cf28
Benny Degezelle <hi@monkeypatch.be>
2015-07-07 23:20:05 +0200

    Move migrations to their proper place (marketplace and form extensions)

--- db/migrate/20120615061510_rename_category_to_categories.rb
+++
@@ -1,9 +0,0 @@
-class RenameCategoryToCategories < ActiveRecord::Migration
-  def self.up
-    rename_column :adverts, :category, :categories
-  end
-
-  def self.down
-    rename_column :adverts, :categories, :category
-  end
-end
--- db/migrate/20120629052335_remove_company_name.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCompanyName < ActiveRecord::Migration
-  def self.up
-    remove_column :adverts, :company_name
-  end
-
-  def self.down
-    add_column :adverts, :company_name, :string
-  end
-end
