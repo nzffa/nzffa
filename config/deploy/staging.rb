636878fcfee446aa4dabfa03aed52990bd368eab
Benny Degezelle <hi@monkeypatch.be>
2015-07-25 14:22:04 +0200

    Update to new staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,10 @@
 set :rails_env, 'staging'
 set :rack_env, rails_env
 set :branch, 'staging'
-set :deploy_to, "/home/#{user}/#{rails_env}"
+set :deploy_to, "/var/www/vhosts/nzffa.avlux.net/#{rails_env}"
+set :user, "nzffa1"
 
-set :solo_host, 'delta.enspiral.info'
+set :solo_host, 'nzffa1.avlux.net'
 role :web, solo_host
 role :app, solo_host
 role :db,  solo_host, :primary => true
