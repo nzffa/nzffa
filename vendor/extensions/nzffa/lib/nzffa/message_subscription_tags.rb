46c7c5ce7a25cc8772b03fdb29e52b11c30a53b5
Benny Degezelle <hi@monkeypatch.be>
2016-04-13 00:50:05 +0200

    Add r:recipient:subscription_print_renewal_url and _link

--- vendor/extensions/nzffa/lib/nzffa/message_subscription_tags.rb
+++ vendor/extensions/nzffa/lib/nzffa/message_subscription_tags.rb
@@ -12,7 +12,7 @@ module Nzffa::MessageSubscriptionTags
   end
   
   desc %{
-    <pre><code><r:recipient:subscription_receipt_link>renew your subscription</r:recipient:subscription_receipt_link></code></pre>
+    <pre><code><r:recipient:subscription_receipt_link>your subscription receipt</r:recipient:subscription_receipt_link></code></pre>
   }
   tag "recipient:subscription_receipt_link" do |tag|
     options = tag.attr.dup
@@ -21,7 +21,25 @@ module Nzffa::MessageSubscriptionTags
     text = tag.double? ? tag.expand : "your subscription receipt"
     %{<a href="#{tag.render('recipient:subscription_receipt_url')}"#{attributes}>#{text}</a>}
   end
+    
+  desc %{
+    <pre><code><r:recipient:subscription_print_renewal_url /></code></pre>
+  }
+  tag "recipient:subscription_print_renewal_url" do |tag|
+    print_renewal_subscriptions_url(:reader_id => tag.locals.recipient.id, :token => tag.locals.recipient.perishable_token, :host => @mailer_vars[:@host])
+  end
   
+  desc %{
+    <pre><code><r:recipient:subscription_print_renewal_link>print your renewal form</r:recipient:subscription_print_renewal_link></code></pre>
+  }
+  tag "recipient:subscription_print_renewal_link" do |tag|
+    options = tag.attr.dup
+    attributes = options.inject('') { |s, (k, v)| s << %{#{k.downcase}="#{v}" } }.strip
+    attributes = " #{attributes}" unless attributes.empty?
+    text = tag.double? ? tag.expand : "print your renewal form"
+    %{<a href="#{tag.render('recipient:subscription_print_renewal_url')}"#{attributes}>#{text}</a>}
+  end
+    
   desc %{
     <pre><code><r:recipient:subscription_renewal_url /></code></pre>
   }
