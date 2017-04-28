baec158d917df3ad0daa5f9ded20a268a851919f
Benny Degezelle <hi@monkeypatch.be>
2017-04-28 18:05:21 +0200

    Update radiant js files

--- public/javascripts/admin/all.js
+++ public/javascripts/admin/all.js
@@ -8162,7 +8162,7 @@ function addField(form) {
   if (validFieldName()) {
     new Ajax.Updater(
       $('attributes').down('tbody'),
-      '/admin/page_fields/',
+      relative_url_root + '/admin/page_fields/',
       {
         asynchronous: true,
         evalScripts: true,
@@ -9179,8 +9179,14 @@ var ShortcutKeysBehavior = Behavior.create({
     var character = String.fromCharCode(event.keyCode);
     if(!event.shiftKey && !character.blank())
       character = character.toLowerCase();
+    // Blindly passing RIGHT_ARROW through fromCharCode() returns a single-quote character (ascii decimal 39).
+    // I suspect that causes a bad string interpolation when evaluating `button`.
+    // Adding double-quotes to the accesskey spec seems to fix it.
+    // Now of course now keyCode 34 (double-quote in ascii decimal) will cause a problem... but your browser will likely catch Ctrl+PgDn anyway
+    // We should only evaluate keyCodes that can come from printable characters, now sure how feasible that is.
+    // Eg. http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
     if(event.ctrlKey && event.keyCode != 17){
-      var button = $$('input[accesskey='+character+']')[0];
+      var button = $$('input[accesskey="'+character+'"]')[0];
       if(button){
         event.stop();
         button.click();
