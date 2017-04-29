bf2a67feadf341122974d8c169ba263fbc06aa2d
Benny Degezelle <hi@monkeypatch.be>
2017-04-29 21:48:51 +0200

    Add support for r:branches order='' by=''

--- vendor/extensions/nzffa/lib/nzffa/branch_tags.rb
+++ vendor/extensions/nzffa/lib/nzffa/branch_tags.rb
@@ -14,8 +14,9 @@ module Nzffa::BranchTags
     <pre><code><r:branches:each:group>...</r:branches:each:group /></code></pre>
   }
   tag "branches:each" do |tag|
+    options = group_find_options(tag)
     result = []
-    tag.locals.branches.each do |branch|
+    tag.locals.branches.all(options).each do |branch|
       tag.locals.branch = branch
       result << tag.expand
     end
@@ -65,5 +66,26 @@ module Nzffa::BranchTags
     attributes = " #{attributes}" unless attributes.empty?
     text = tag.double? ? tag.expand : url
     %{<a href="#{url}"#{attributes}>#{text}</a>}
+  end
+
+  def group_find_options tag
+    attr = tag.attr.symbolize_keys
+    options = {}
+    
+    by = (attr[:by] || 'name').strip
+    order = (attr[:order] || 'asc').strip
+    order_string = ''
+    if Group.new.attributes.keys.include?(by)
+      order_string << by
+    else
+      raise TagError.new("`by' attribute of `each' tag must be set to a valid field name")
+    end
+    if order =~ /^(asc|desc)$/i
+      order_string << " #{$1.upcase}"
+    else
+      raise TagError.new(%{`order' attribute of `each' tag must be set to either "asc" or "desc"})
+    end
+    options[:order] = order_string
+    options
   end
 end
