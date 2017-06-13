cf02c84448b90c87ab285515983803ee027c3e27
Benny Degezelle <hi@monkeypatch.be>
2017-06-13 15:41:14 +0200

    Remove r:id from nzffa extension
    This functionality has been moved into the find_by_id_tag extension.

--- vendor/extensions/nzffa/nzffa_extension.rb
+++ vendor/extensions/nzffa/nzffa_extension.rb
@@ -31,7 +31,6 @@ class NzffaExtension < Radiant::Extension
     Page.send :include, Nzffa::IfDescendantOrSelfTags
     Page.send :include, Nzffa::BranchTags
     Page.send :include, Nzffa::NewsletterLinkTags
-    Page.send :include, Nzffa::IdTag
     
     admin.reader.index.add :thead, "ids_ths", :before => "title_header"
     admin.reader.index.add :tbody, "ids_tds", :before => "title_cell"
