3c2354db9d5f94849899b8def39964b8af14fe1a
Benny Degezelle <hi@monkeypatch.be>
2017-06-14 15:41:19 +0200

    Disable cron jobs for rake task that are no longer available

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@ set :job_template, nil
 
 every 1.day do
   rake 'radiant:extensions:marketplace:email_warnings'
-  rake 'radiant:extensions:marketplace:subscription_email_warnings'
-  rake 'radiant:extensions:marketplace:subscription_expiry'
+  # rake 'radiant:extensions:marketplace:subscription_email_warnings'
+  # rake 'radiant:extensions:marketplace:subscription_expiry'
 end
