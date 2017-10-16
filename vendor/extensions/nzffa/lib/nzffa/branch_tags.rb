03ba0d2fabd976e6805ceed358147a9c50f6ddc5
Benny Degezelle <hi@monkeypatch.be>
2017-10-16 23:58:50 +0200

    Flesh out branch tags; also allow <r:branch id=''> and reader access to president, treasurer, ...

--- vendor/extensions/nzffa/lib/nzffa/branch_tags.rb
+++ vendor/extensions/nzffa/lib/nzffa/branch_tags.rb
@@ -23,34 +23,69 @@ module Nzffa::BranchTags
     result.join ''
   end
   
+  desc %{
+    Sets the branch scope the branch with the id specified in the id attribute.
+
+    <pre><code><r:branch id="1">...</r:branch></code></pre>
+  }
+  tag "branch" do |tag|
+    if id = tag.attr.delete('id')
+      tag.expand if tag.locals.branch = Group.find(id)
+    else
+      tag.expand if tag.locals.branch
+    end
+  end
+  
+  desc %{
+    Sets the group scope to the current branch.
+
+    <pre><code><r:branch id="1"><r:group>...</r:group></r:branch></code></pre>
+    
+    or
+    
+    <pre><code><r:branches:each:group>...</r:branches:each:group></code></pre>
+  }
   tag "branches:each:group" do |tag|
     tag.locals.group = tag.locals.branch
     tag.expand
   end
+  tag "branch:group" do |tag|
+    tag.locals.group = tag.locals.branch
+    tag.expand
+  end
   desc %{
-    Sets the page scope to the current branch's group's homepage.
-
-    <pre><code><r:branches:each:group:homepage>...</r:branches:each:group:homepage></code></pre>
+    Sets the page scope to the current group's homepage.
+    
+    <pre><code><r:group:homepage>...</r:group:homepage></code></pre>
   }
-  tag "branches:each:group:homepage" do |tag|
+  tag "group:homepage" do |tag|
     tag.locals.page = tag.locals.group.homepage
     tag.expand
   end
   
-  desc %{
-    Sets the reader scope to the current branch's secretary.
-    The reader id of the secretary is to be set in a page field 'secretary_reader_id' on the homepage of this branch
-
-    <pre><code><r:branches:each:secretary:reader>...</r:branches:each:secretary:reader></code></pre>
-  }
-  tag "branches:each:secretary" do |tag|
-    id = tag.locals.branch.homepage.try(:field, 'secretary_reader_id')
-    tag.expand if id && tag.locals.secretary = Reader.find(id)
-  end
+  %w(secretary president treasurer councillor newsletter_editor).each do |role|
+    desc %{
+      Sets the reader scope to the current branch's #{role}.
+      The reader id of the #{role} is to be set in a page field '#{role}_reader_id' on the homepage of this branch
+      <pre><code><r:branches:each:#{role}:reader>...</r:branches:each:#{role}:reader></code></pre>
+    }
+    tag "branches:each:#{role}" do |tag|
+      id = tag.locals.branch.homepage.try(:field, role + '_reader_id')
+      tag.expand if id && tag.locals.send("#{role}=", Reader.find(id))
+    end
+    tag "branch:#{role}" do |tag|
+      id = tag.locals.branch.homepage.try(:field, role + '_reader_id')
+      tag.expand if id && tag.locals.send("#{role}=", Reader.find(id))
+    end
+    tag "branches:each:#{role}:reader" do |tag|
+      tag.locals.reader = tag.locals.send(role)
+      tag.expand
+    end
+    tag "branch:#{role}:reader" do |tag|
+      tag.locals.reader = tag.locals.send(role)
+      tag.expand
+    end
   
-  tag "branches:each:secretary:reader" do |tag|
-    tag.locals.reader = tag.locals.secretary
-    tag.expand
   end
   
   desc %{
@@ -60,6 +95,17 @@ module Nzffa::BranchTags
     <pre><code><r:branches:each:admin_link>link text</r:branches:each:admin_link></code></pre>
   }
   tag "branches:each:admin_link" do |tag|
+    # Can not do this because tag.double?
+    # tag.render('branch:admin_link', tag.attr.dup)
+    # So just duplicate code..:
+    url = "/branch_admin/#{tag.locals.branch.id}"
+    options = tag.attr.dup
+    attributes = options.inject('') { |s, (k, v)| s << %{#{k.downcase}="#{v}" } }.strip
+    attributes = " #{attributes}" unless attributes.empty?
+    text = tag.double? ? tag.expand : url
+    %{<a href="#{url}"#{attributes}>#{text}</a>}
+  end
+  tag "branch:admin_link" do |tag|
     url = "/branch_admin/#{tag.locals.branch.id}"
     options = tag.attr.dup
     attributes = options.inject('') { |s, (k, v)| s << %{#{k.downcase}="#{v}" } }.strip
