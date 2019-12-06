dc1effcb646ee50faf775a6910237d2354b8fcab
jomz <hi@monkeypatch.be>
2019-12-06 05:50:59 +0100

    Undo screw-up in LC..

--- vendor/extensions/nzffa/app/models/order.rb
+++ vendor/extensions/nzffa/app/models/order.rb
@@ -173,10 +173,10 @@ class Order < ActiveRecord::Base
     invoice = @gateway.build_invoice({
       :invoice_type => "ACCREC",
       :invoice_status => "AUTHORISED",
-      :date => o.created_at.to_date,
-      :due_date => (o.created_at + 1.month),
-      :invoice_number => o.id,
-      :reference => "Member ID #{o.reader.nzffa_membership_id}",
+      :date => created_at.to_date,
+      :due_date => (created_at + 1.month),
+      :invoice_number => id,
+      :reference => "Member ID #{reader.nzffa_membership_id}",
       :line_amount_types => "Inclusive"
     })
     invoice.contact.name = reader.name
