f7cf6c5f4bc9af1ffae43cc3daabf614afa9a6f0
Benny Degezelle <benny@morse.be>
2022-06-27 15:42:33 +0200

    Avoid double spaces in reader.name; Xero chokes on them when searching or creating a Contact

--- vendor/extensions/nzffa/lib/reader_mixin.rb
+++ vendor/extensions/nzffa/lib/reader_mixin.rb
@@ -67,7 +67,7 @@ module ReaderMixin
 
   def name
     if forename.present? and surname.present?
-      "#{forename.strip} #{surname.strip}"
+      "#{forename} #{surname}".squeeze # avoid double spaces as Xero chokes on them
     else
       nil
     end
