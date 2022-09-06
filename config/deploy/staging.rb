f1b066ebccfb4d220813b89c8f51ce072fcc4998
Benny Degezelle <benny@morse.be>
2022-09-06 10:54:55 +0200

    Deploy from 'staging' branch again

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :rails_env, 'staging'
 set :rack_env, rails_env
 set :bundle_cmd, '/home/nzffa-staging-app/.rbenv/shims/bundle'
 set :whenever_command, "/home/nzffa-staging-app/.rbenv/shims/bundle exec whenever"
-set :branch, 'with_rails_lts'
+set :branch, 'staging'
 set :user, 'nzffa-staging-app'
 set :deploy_to, "/home/#{user}/#{rails_env}"
 
